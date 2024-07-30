621d3c9cd978c69c03d827da0d2e73eb3dfe4a27
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-07-31 03:08:39 +0500

    updated

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express=require("express");
-const app =express();
-const port = 3600;
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.get( "/Home",(req, res)=>{
-      res.send("This is empty response..!!");
+app.get("/Home", (req, res) => {
+    res.send("This is an empty response..!!");
 });
-app.get("/ShaibiCode", (req, res)=>{
+
+app.get("/ShaibiCode", (req, res) => {
     res.send("Hi there in ShaibiCode and Solution");
 });
-app.listen (port,()=>{
-    console.log(`ShaibiCode and Solution is running on ${port}`);
+
+const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
+
+app.listen(PORT, () => {
+    console.log(`ShaibiCode and Solution is running on ${PORT}`);
 });
